d71caca29dec827118c9f042238051f4c9b7462a
Allan Tokuda <praetis@gmail.com>
2014-05-18 17:22:53 -0500

    Cleanup

--- js/file-drop-zone.js
+++ js/file-drop-zone.js
@@ -8,15 +8,11 @@ angular.module('metaApp').directive('fileDropZone', function() {
       fileName: '='
     },
     link: function(scope, element, attrs) {
-      var checkSize, isTypeValid, processDragOverOrEnter, validMimeTypes;
-      processDragOverOrEnter = function(event) {
-        if (event != null) {
-          event.preventDefault();
-        }
-        event.dataTransfer.effectAllowed = 'copy';
-        return false;
-      };
+      var checkSize, isTypeValid, validMimeTypes;
+
+      //Use the value of the directive to specify a list of valid mime types
       validMimeTypes = attrs.fileDropzone;
+
       checkSize = function(size) {
         var _ref;
         if (((_ref = attrs.maxFileSize) === (void 0) || _ref === '') || (size / 1024) / 1024 < attrs.maxFileSize) {
@@ -26,6 +22,7 @@ angular.module('metaApp').directive('fileDropZone', function() {
           return false;
         }
       };
+
       isTypeValid = function(type) {
         if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
           return true;
@@ -34,11 +31,9 @@ angular.module('metaApp').directive('fileDropZone', function() {
           return false;
         }
       };
-      element.bind('dragover', processDragOverOrEnter);
-      element.bind('dragenter', processDragOverOrEnter);
+
       return element.bind('drop', function(event) {
         var file, name, reader, size, type;
-        console.log('yes');
         if (event != null) {
           event.preventDefault();
         }
